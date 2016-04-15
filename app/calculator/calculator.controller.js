9bfa5f9b45b09e7609521436aae71a17179fcc92
Dan Gallagher <dgallagh9999@gmail.com>
2016-04-15 12:36:20 -0400

    Debug calculator input with state issues

--- app/calculator/calculator.controller.js
+++ app/calculator/calculator.controller.js
@@ -6,48 +6,80 @@ angular
   .controller('calculatorController', calculator)
 
 function calculator ($scope, dataService) {
+  //route the dataService to a local object
   $scope.dataService = dataService
-
   // create containers for top and bottom line elements
   $scope.displayBot = ''
   $scope.displayTop = ''
-
   // create container for current equation
   $scope.calc = []
+  // create state vaiable for equation solution
+  $scope.equationSolved = false
+
+  // Add clicked button to the display
+  $scope.inputNumberClick = function (btn) {
+    if ($scope.equationSolved) { return }
+    $scope.displayBot += (btn)
+  }
+
+  // Add value and clicked function to the display
+  $scope.inputFunctionClick = function (fn) {
+    // Check if blank or isNaN
+    if ($scope.displayBot === '' || isNaN($scope.displayBot)) {
+      return
+    // Toggle '-' number
+    } else if (fn === '+/-') {
+      $scope.displayBot = toggleLeadingChar($scope.displayBot.toString(), '-')
+      $scope.equationSolved = false
+    // Toggle '√' number
+    } else if (fn === '√') {
+      $scope.displayBot = toggleLeadingChar($scope.displayBot.toString(), '√')
+      $scope.equationSolved = false
+    } else {
+      $scope.calc.push($scope.displayBot)
+      $scope.calc.push(fn)
+      displayEquation($scope.calc)
+      $scope.clear('bottom')
+      $scope.equationSolved = false
+    }
+  }
+
+  // Add a clear function
+  $scope.clear = function (what) {
+    switch (what) {
+      case 'all':
+        $scope.calc = []
+        $scope.displayTop = ''
+        $scope.displayBot = ''
+        $scope.equationSolved = false
+        break
+      case 'top':
+        $scope.displayTop = ''
+        break
+      case 'bottom':
+        $scope.displayBot = ''
+        break
+      case 'array':
+        $scope.calc = []
+        break
+    }
+  }
 
   // Handle the equals click to evaluate expression and return result
   $scope.equate = function () {
     // check to see if equation has been solved
-    if ($scope.calc[$scope.calc.length - 1] === '=') {
-      return
-    }
+    if ($scope.equationSolved || isNaN($scope.displayBot)) { return }
+    // Add last number entered and '=' to the calc array
     $scope.calc.push($scope.displayBot, '=')
-    $scope.displayBot = ''
-    // clean equation
+    // Clear the display bottom
+    $scope.clear('bottom')
+    displayEquation($scope.calc)
+    // Clean the calc array equation
     $scope.calc = cleanArray($scope.calc)
-    displayEquation($scope.calc)
+    // Solve the array equation and display answer on display bottom
     $scope.displayBot = solveArray($scope.calc)
-  }
-
-  // Add clicked button to the display
-  $scope.inputNumberClick = function (btn) {
-      $scope.displayBot += (btn)
-  }
-
-  // Add clicked button to the display
-  $scope.inputFunctionClick = function (fn) {
-    if (fn === '+/-') {
-      $scope.displayBot = toggleLeadingChar($scope.displayBot, '-')
-    } else if (fn === '√') {
-      $scope.displayBot = toggleLeadingChar($scope.displayBot, '√')
-    } else if ($scope.displayBot === '') {
-      return
-    } else {
-      $scope.calc.push($scope.displayBot)
-      $scope.calc.push(fn)
-      $scope.displayBot = ''
-    }
-    displayEquation($scope.calc)
+    $scope.clear('array')
+    $scope.equationSolved = true
   }
 
   // Toggle a charater on or off from a string
@@ -57,7 +89,10 @@ function calculator ($scope, dataService) {
   }
 
   // Convert calc array to a string and display it
-  function displayEquation (inputArray) {
+  function displayEquation (inputArray, addItem) {
+    if (addItem) {
+      inputArray.push(addItem)
+    }
     $scope.displayTop = inputArray.join(' ')
   }
 
@@ -84,7 +119,7 @@ function calculator ($scope, dataService) {
       fnAnswer = Math.sqrt(parseFloat(equationArray[fnIndex].slice(1)))
       equationArray.splice(fnIndex, 1, fnAnswer)
     }
-
+    
     // Check to see if solution has been found
     if (equationArray.length === 1) { return equationArray[0] }
 
@@ -159,43 +194,4 @@ function calculator ($scope, dataService) {
       console.log('Data not vailid for new card')
     }
   }
-
-/* New plan for calculator function
-keypad will except numbers only for screen to not change
-functions will shift the number on the lower display up and add the function
-  to the upper line such as "36 +"
-+/- function will change the number on screen to its negative
-sqrt will evaluate automaticly
-once = is hit the array will be parsed and the equation solved
-drop last function added to evaluate such as "36 + 73 +"
-default for the list button is disabled
-after an evaluate the list button is enabled
-after a clear function the list button is disabled
-
-new functions
-solveArray
-  if = is at the end of the array return null
-  map functions to js operators
-  create equation from the array
-  add = to the end of the array
-  return results
-functionClick
-  add number entered to calc array
-  add function symbol to array
-  run display array
-displayArray
-  pasre array into a string to display on the upper line
-
---before eval--
-if ^
-  split ' '
-  search for index of ^
-  set base to index - 1
-  set exp to index + 1
-  set index of ^ = Math.POW(base,pow)
-  splice index -1
-  splice index +1
-  join ' '
---then eval--
-*/
 }
